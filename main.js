e89e713a4b679e9d2af640a98c325aca84801af6
Austin <austineady@github.com>
2015-05-14 17:35:24 -0400

    calculator commit

--- main.js
+++ main.js
@@ -1,60 +1,56 @@
-var keys = document.querySelectorAll('li');
-var operators = ['+', '-', 'x', '/'];
-var decimalAdded = false;
+var instructionQueue = [];
+var equationInput = instructionQueue.join('')
+var equationAnswer = eval(equationInput)
 
-for(var i = 0; i < keys.length; i++) {
-  keys[i].onclick = function(e) {
-    var input = document.querySelector('.display');
-    var inputVal = input.innerHTML;
-    var btnVal = this.innerHTML;
-
-    if(btnVal == 'C') {
-      input.innerHTML = ";
-      decimalAdded = false;
+function alertNumber(event) {
+  var button = event.target;
+  var number = Number(button.textContent);
+  var lastpressed = instructionQueue.push(number);
+  document.getElementById("javaoutput").innerHTML = document.getElementById("javaoutput").innerHTML + number
 }
-    else if(btnVal == '=') {
-      var equation = inputVal;
-      var lastChar = equation[equation.length - 1];
+
+[].forEach.call(document.querySelectorAll('.number'), function(element) {
+  element.addEventListener('click', alertNumber);
+}, false);
 
 
-    equation = equation.replace(/x/g,'*');
-    if(operator.indexOf(lastChar) > -1 || lastChar == '.')
-      equation = equation.replace(/.$/,'');
-
-
-    if(equation)
-      input.innerHTML = eval(equation);
-
-
-    decimalAdded = false;
+function operatorPressed(event) {
+  var button = event.target;
+  var text = button.textContent;
+  var lastoperator = instructionQueue.push(text);
+  document.getElementById("javaoutput").innerHTML = document.getElementById("javaoutput").innerHTML + text
 }
-    else if(operators.indexOf(btnVal) > -1) {
-      var lastChar = inputVal[inputVal.length - 1];
 
+[].forEach.call(document.querySelectorAll('.operator'), function(element) {
+  element.addEventListener('click', operatorPressed);
+}, false);
 
-    if(inputVal !=" && operators.indexOf(lastChar) == -1)
-      input.innerHTML += btnVal;
-
-
-    else if(inputVal ==" && btnVal =='-')
-      input.innerHTML += btnVal;
-
-
-    if(operators.indexOf(lastChar) > -1 && inputVal.length > 1) {
-      input.innerHTML = inputVal.replace(/.$/, btnVal);
+function equalPressed(event) {
+  var button = event.target;
+  var text = button.textContent;
+  var equationInput = instructionQueue.join('')
+  var equationAnswer = eval(equationInput)
+  document.getElementById("javaoutput").innerHTML = equationAnswer
 }
-    decimalAdded = false;
-}
-    else if(btnVal == '.') {
-      if(!decimalAdded) {
-        input.innerHTML += btnVal;
-        decimalAdded = true;
-  }
-}
-    else {
-      input.innerHTML += btnVal;
-    }
 
-    e.preventDefault();
-  }
+[].forEach.call(document.querySelectorAll('.equal'), function(element) {
+  element.addEventListener('click', equalPressed);
+}, false);
+
+function clearPressed(event) {
+  var button = event.target;
+  document.getElementById("javaoutput").innerHTML = "";
 }
+
+[].forEach.call(document.querySelectorAll('.clear'), function(element) {
+  element.addEventListener('click', clearPressed);
+}, false);
+
+function signPressed(event) {
+  var button = event.target;
+  var signChange = instructionQueue.push(-1);
+}
+
+[].forEach.call(document.querySelectorAll('.sign'), function(element) {
+  element.addEventListener('click', clearPressed);
+}, false);
