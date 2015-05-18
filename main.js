6b39446d663acf28ba186e439bf38d80b2fd621a
Austin <austineady@github.com>
2015-05-18 15:35:10 -0400

    final calc. decimal not working

--- main.js
+++ main.js
@@ -1,227 +1,193 @@
-var instructionQueue = [];
-var equationInput = instructionQueue.join('')
-var equationAnswer = eval(equationInput)
-var piNumber = 3.14159
+/* Variables */
 
-function alertNumber(event) {
-  var button = event.target;
-  var number = Number(button.textContent);
-  var lastpressed = instructionQueue.push(number);
-  var equationInput = instructionQueue.join('')
-  document.getElementById("javaoutput").innerHTML = equationInput
+//To set up the HTML display, where .display is the class name
+var $display = document.querySelector('.display');
+var calculation = 0;
+var displayString = "0";
+var pendingOperation;
+var nthOperation;
+
+
+//Event handler functions
+function numberPressed(event) {
+  var inputNumber = Number(event.target.textContent);
+  if (pendingOperation !== undefined) {
+    switch (pendingOperation) {
+      case "+":
+        calculation += inputNumber;
+        break;
+      case "-":
+        calculation -= inputNumber;
+        break;
+      case "*":
+        calculation *= inputNumber;
+        break;
+      case "/":
+        calculation /= inputNumber;
+        break;
+      case "^n":
+        calculation = Math.pow(calculation, inputNumber);
+        break;
+      case "^1/n":
+        calculation = Math.pow(calculation, (1/inputNumber));
+        break;
+    }
+    pendingOperation = undefined;
+    $display.textContent = inputNumber;
+  } else {
+    displayOrContatenateNumber(inputNumber);
+  }
 }
-
-[].forEach.call(document.querySelectorAll('.number'), function(element) {
-  element.addEventListener('click', alertNumber);
-}, false);
-
 
 function operatorPressed(event) {
-  var button = event.target;
-  var text = button.textContent;
-  var lastoperator = instructionQueue.push(text);
-  var equationInput = instructionQueue.join('')
+  pendingOperation = event.target.textContent;
+  $display.textContent = calculation;
 }
+
+function equalPressed(event) {
+  $display.textContent = calculation;
+}
+
+function clearPressed(event) {
+  calculation = 0;
+  displayString = 0;
+  percentChange = 0;
+  $display.textContent = calculation;
+}
+
+function percentPressed(event) {
+  calculation = calculation/100;
+  $display.textContent = calculation;
+}
+
+function signPressed(event) {
+  calculation = calculation*-1;
+  $display.textContent = calculation;
+}
+
+function decimalPressed(event) {
+  var decimalChange = String(calcuation) + 0.00;
+  calculation = Number(decimalChange);
+  $display.textContent = calculation;
+}
+
+function squaredPressed(event) {
+  calculation = Math.pow(calculation, 2);
+  $display.textContent = calculation;
+}
+
+function cubedPressed(event) {
+  calculation = Math.pow(calculation, 3);
+  $display.textContent = calculation;
+}
+
+function npowerPressed(event) {
+  pendingOperation = event.target.textContent;
+}
+
+function squareRootPressed(event) {
+  calculation = Math.pow(calculation, .5);
+  $display.textContent = calculation;
+}
+
+function cubeRootPressed(event) {
+  calculation = Math.pow(calculation, (1/3));
+  $display.textContent = calculation;
+}
+
+function nrootPressed(event) {
+  pendingOperation = event.target.textContent;
+}
+
+function sinPressed(event) {
+  calculation = Math.sin(calculation);
+  $display.textContent = calculation;
+}
+
+function cosPressed(event) {
+  calculation = Math.cos(calculation);
+  $display.textContent = calculation;
+}
+
+function tanPressed(event) {
+  calculation = Math.tan(calculation);
+  $display.textContent = calculation;
+}
+
+function sinhPressed(event) {
+  calculation = Math.sinh(calculation);
+  $display.textContent = calculation;
+}
+
+function coshPressed(event) {
+  calculation = Math.cosh(calculation);
+  $display.textContent = calculation;
+}
+
+function tanhPressed(event) {
+  calculation = Math.tanh(calculation);
+  $display.textContent = calculation;
+}
+
+function logPressed(event) {
+  calculation = Math.log(calculation);
+  $display.textContent = calculation;
+}
+
+function piPressed(event) {
+  calculation = calculation*3.14159;
+  $display.textContent = calculation;
+}
+
+function ekeyPressed(event) {
+  calculation = calculation*2.71828;
+  $display.textContent = calculation;
+}
+
+
+//Utility Functions
+function displayOrContatenateNumber(inputNumber) {
+  if (calculation === 0) {
+    calculation = Number(inputNumber);
+    displayString = String(inputNumber);
+  } else {
+    calculation = Number(displayString + inputNumber);
+    displayString = String(displayString + inputNumber);
+  }
+  $display.textContent = displayString;
+}
+
+
+//Setup event handlers
+[].forEach.call(document.querySelectorAll('.number'), function(element) {
+  element.addEventListener('click', numberPressed);
+}, false);
 
 [].forEach.call(document.querySelectorAll('.operator'), function(element) {
   element.addEventListener('click', operatorPressed);
 }, false);
 
-function equalPressed(event) {
-  var button = event.target;
-  var text = button.textContent;
-  var equationInput = instructionQueue.join('')
-  var equationAnswer = eval(equationInput)
-  document.getElementById("javaoutput").innerHTML = equationAnswer
-  instructionQueue = [equationAnswer];
-}
-
-[].forEach.call(document.querySelectorAll('.equal'), function(element) {
-  element.addEventListener('click', equalPressed);
+[].forEach.call(document.querySelectorAll('.scikey'), function(element) {
+  element.addEventListener('click', scikeyPressed);
 }, false);
 
-function clearPressed(event) {
-  var button = event.target;
-  instructionQueue = []
-  document.getElementById("javaoutput").innerHTML = "";
-}
-
-[].forEach.call(document.querySelectorAll('.clear'), function(element) {
-  element.addEventListener('click', clearPressed);
-}, false);
-
-function signPressed(event) {
-  var button = event.target;
-  var numberManipulate = instructionQueue.pop();
-  var numberChange = numberManipulate * -1
-  instructionQueue.push(numberChange)
-  document.getElementById("javaoutput").innerHTML = instructionQueue
-}
-
-[].forEach.call(document.querySelectorAll('.sign'), function(element) {
-  element.addEventListener('click', signPressed);
-}, false);
-
-function percentPressed(event) {
-  var button = event.target;
-  var numberManipulate = instructionQueue.pop();
-  var percentChange = numberManipulate / 100
-  instructionQueue.push(percentChange)
-  document.getElementById("javaoutput").innerHTML = instructionQueue
-}
-
-[].forEach.call(document.querySelectorAll('.percent'), function(element) {
-  element.addEventListener('click', percentPressed);
-}, false);
-
-function squarePressed(event) {
-  var button = event.target;
-  var numberManipulate = instructionQueue.pop();
-  var powerChange = Math.pow(numberManipulate, 2)
-  instructionQueue.push(powerChange)
-  document.getElementById("javaoutput").innerHTML = instructionQueue
-}
-
-[].forEach.call(document.querySelectorAll('.squared'), function(element) {
-  element.addEventListener('click', squarePressed);
-}, false);
-
-function cubedPressed(event) {
-  var button = event.target;
-  var numberManipulate = instructionQueue.pop();
-  var powerChange = Math.pow(numberManipulate, 3)
-  instructionQueue.push(powerChange)
-  document.getElementById("javaoutput").innerHTML = instructionQueue
-}
-
-[].forEach.call(document.querySelectorAll('.cubed'), function(element) {
-  element.addEventListener('click', cubedPressed);
-}, false);
-
-// function nPowerPressed(event) {
-//   var button = event.target;
-//   var numberManipulate = instructionQueue.pop();
-//   var powerChange = Math.pow(numberManipulate,
-//   instructionQueue.push(powerChange)
-// }
-//
-// [].forEach.call(document.querySelectorAll('.npower'), function(element) {
-//   element.addEventListener('click', nPowerPressed);
-// }, false);
-
-function squarerootPressed(event) {
-  var button = event.target;
-  var numberManipulate = instructionQueue.pop();
-  var powerChange = Math.pow(numberManipulate, .5)
-  instructionQueue.push(powerChange)
-  document.getElementById("javaoutput").innerHTML = instructionQueue
-}
-
-[].forEach.call(document.querySelectorAll('.squareroot'), function(element) {
-  element.addEventListener('click', squarerootPressed);
-}, false);
-
-function cuberootPressed(event) {
-  var button = event.target;
-  var numberManipulate = instructionQueue.pop();
-  var powerChange = Math.pow(numberManipulate, (1/3))
-  instructionQueue.push(powerChange)
-  document.getElementById("javaoutput").innerHTML = instructionQueue
-}
-
-[].forEach.call(document.querySelectorAll('.cuberoot'), function(element) {
-  element.addEventListener('click', cuberootPressed);
-}, false);
-
-function sinPressed(event) {
-  var button = event.target;
-  var numberManipulate = instructionQueue.pop();
-  var angleChange = Math.sin(numberManipulate);
-  instructionQueue.push(angleChange)
-  document.getElementById("javaoutput").innerHTML = instructionQueue
-}
-
-[].forEach.call(document.querySelectorAll('.sin'), function(element) {
-  element.addEventListener('click', sinPressed);
-}, false);
-
-function cosPressed(event) {
-  var button = event.target;
-  var numberManipulate = instructionQueue.pop();
-  var angleChange = Math.cos(numberManipulate);
-  instructionQueue.push(angleChange)
-  document.getElementById("javaoutput").innerHTML = instructionQueue
-}
-
-[].forEach.call(document.querySelectorAll('.cos'), function(element) {
-  element.addEventListener('click', cosPressed);
-}, false);
-
-function tanPressed(event) {
-  var button = event.target;
-  var numberManipulate = instructionQueue.pop();
-  var angleChange = Math.tan(numberManipulate);
-  instructionQueue.push(angleChange)
-  document.getElementById("javaoutput").innerHTML = instructionQueue
-}
-
-[].forEach.call(document.querySelectorAll('.tan'), function(element) {
-  element.addEventListener('click', tanPressed);
-}, false);
-
-function sinhPressed(event) {
-  var button = event.target;
-  var numberManipulate = instructionQueue.pop();
-  var angleChange = Math.sinh(numberManipulate);
-  instructionQueue.push(angleChange)
-  document.getElementById("javaoutput").innerHTML = instructionQueue
-}
-
-[].forEach.call(document.querySelectorAll('.sinh'), function(element) {
-  element.addEventListener('click', sinhPressed);
-}, false);
-
-function coshPressed(event) {
-  var button = event.target;
-  var numberManipulate = instructionQueue.pop();
-  var angleChange = Math.cosh(numberManipulate);
-  instructionQueue.push(angleChange)
-  document.getElementById("javaoutput").innerHTML = instructionQueue
-}
-
-[].forEach.call(document.querySelectorAll('.cosh'), function(element) {
-  element.addEventListener('click', coshPressed);
-}, false);
-
-function tanhPressed(event) {
-  var button = event.target;
-  var numberManipulate = instructionQueue.pop();
-  var angleChange = Math.tanh(numberManipulate);
-  instructionQueue.push(angleChange)
-  document.getElementById("javaoutput").innerHTML = instructionQueue
-}
-
-[].forEach.call(document.querySelectorAll('.tanh'), function(element) {
-  element.addEventListener('click', tanhPressed);
-}, false);
-
-function piPressed(event) {
-  var button = event.target;
-  instructionQueue.push(piNumber)
-  document.getElementById("javaoutput").innerHTML = instructionQueue
-}
-
-[].forEach.call(document.querySelectorAll('.pi'), function(element) {
-  element.addEventListener('click', piPressed);
-}, false);
-
-function ekeyPressed(event) {
-  var button = event.target;
-  var eNumber = 2.718281828
-  instructionQueue.push(eNumber)
-  document.getElementById("javaoutput").innerHTML = instructionQueue
-}
-
-[].forEach.call(document.querySelectorAll('.ekey'), function(element) {
-  element.addEventListener('click', ekeyPressed);
-}, false);
+document.querySelector('.equal').addEventListener('click', equalPressed);
+document.querySelector('.clear').addEventListener('click', clearPressed);
+document.querySelector('.percent').addEventListener('click', percentPressed);
+document.querySelector('.sign').addEventListener('click', signPressed);
+// document.querySelector('.decimal').addEventListener('click', decimalPressed);
+document.querySelector('.squared').addEventListener('click', squaredPressed);
+document.querySelector('.cubed').addEventListener('click', cubedPressed);
+document.querySelector('.npower').addEventListener('click', npowerPressed);
+document.querySelector('.squareroot').addEventListener('click', squareRootPressed);
+document.querySelector('.cuberoot').addEventListener('click', cubeRootPressed);
+document.querySelector('.nroot').addEventListener('click', nrootPressed);
+document.querySelector('.sin').addEventListener('click', sinPressed);
+document.querySelector('.cos').addEventListener('click', cosPressed);
+document.querySelector('.tan').addEventListener('click', tanPressed);
+document.querySelector('.sinh').addEventListener('click', sinhPressed);
+document.querySelector('.cosh').addEventListener('click', coshPressed);
+document.querySelector('.tanh').addEventListener('click', tanhPressed);
+document.querySelector('.log').addEventListener('click', logPressed);
+document.querySelector('.pi').addEventListener('click', piPressed);
+document.querySelector('.ekey').addEventListener('click', ekeyPressed);
